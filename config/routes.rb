4993bf0c68bb822844656faf3d7849854b90c240
sandeepdhakarwal <sandeepdhakarwal01@gmail.com>
2022-01-31 23:21:28 +0530

    add page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+ 
   root to: 'pages#index'
 end
